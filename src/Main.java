44b94891fc05d9269ee90de3329cb10eb992d97c
vkostenko <git@github.com:TheViktorSAy/Hellow-World.git>
2024-10-08 09:43:20 +0300

    Корректировка 2ого задания и форматирование кода для лучшей читаемости

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,21 @@ public class Main {
 
         System.out.println("Задание 1");
 
-        String firstName = " Ivan" ;
-        String middleName = " Ivanovich" ;
-        String lastName = " Ivanov" ;
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov";
         String fullName = lastName + firstName + middleName;
         System.out.println("Ф.И.О. сотрудника - " + fullName);
 
         System.out.println("Задание 2");
         String small = "ivanov ivan ivanovich";
-        System.out.println(small.toUpperCase());
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + small.toUpperCase());
 
         System.out.println("Задание 3");
 
         String fullNameRus = "Иванов Семён Семёнович";
         String fullNameRus2 = fullNameRus.replace('ё', 'е');
         System.out.println(fullNameRus2);
-
-
 
 
     }
